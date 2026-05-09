a6ecd938bbdfdcc632d16ad2ce10c72ef4ed08c2
Bruno Torres <brunorafael72@outlook.com>
2026-05-09 19:38:21 -0300

    dynamic arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,22 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // Create an array that will store all multiples.
+        // The size of the array should match the length parameter.
+        double[] result = new double[length];
+
+
+        // Use a loop to go through each position in the array.
+        // The loop starts at 0 and ends at length -1
+        for (int i = 0; i < length; i++)
+        {
+            // Calculate the multiple of the number.
+            // Since arrays start at index 0, we need to use (i + 1) to get the correct multiple.
+            // so the first multiple will be number * 1
+            result[i] = number * (i + 1);
+        }
+
+        return result; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +44,30 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // Create a temporary list to store the rotated values.
+        List<int> rotated = new List<int>();
+
+        // Add the last "amount" values from the original list to the temporary list.
+        // to the beginning of the new list.
+        for (int i = data.Count - amount; i < data.Count; i++)
+        {
+            rotated.Add(data[i]);
+        }
+
+        // Add the remaining values from the beginning of the original list
+        for (int i = 0; i < data.Count - amount; i++)
+        {
+            rotated.Add(data[i]);
+        }
+
+        // Clean the list
+        data.Clear();
+
+        // Replace the original list values with the rotated values.
+        foreach (int value in rotated)
+        {
+            data.Add(value);
+        }
     }
 }
